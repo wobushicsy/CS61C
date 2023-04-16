92a04a2e792dece439e2c4e9fd2bdbe099191f40
wobushicsy <csy657703828@163.com>
2023-04-16 11:33:01 +0800

    read_matrix done

--- su20-proj2-starter/src/read_matrix.s
+++ su20-proj2-starter/src/read_matrix.s
@@ -26,16 +26,95 @@
 read_matrix:
 
     # Prologue
-	
+	# push ra, s0, s1, s2, s3, s4, s5, s6
+    addi sp, sp, -32
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+    sw s2, 12(sp)
+    sw s3, 16(sp)
+    sw s4, 20(sp)
+    sw s5, 24(sp)
+    sw s6, 28(sp)
 
+    # save integer pointer
+    mv s1, a1
+    mv s2, a2
 
+    # call fopen
+    mv a1, a0
+    mv a2, x0
+    jal ra, fopen
 
+    # save file descriptor
+    mv s0, a0
 
+    # s3 = (int*) row
+    # test success
+    li a0, 4
+    jal ra, malloc
+    mv s3, a0
+    mv a1, s0
+    mv a2, s3
+    li a3, 4
+    jal ra, fread
 
+    ##############################
+    # test:                      #
+    # lw a1, 0(s3)               #
+    # jal ra, print_int          #
+    ##############################
 
+    # s4 = (int*) column
+    # test pass
+    li a0, 4
+    jal ra, malloc
+    mv s4, a0
+    mv a1, s0
+    mv a2, s4
+    li a3, 4
+    jal ra, fread
+
+    ##############################
+    # test:                      #
+    # lw a1, 0(s4)               #
+    # jal ra, print_int          #
+    ##############################
+
+    # calculate total needed memory 
+    lw t0, 0(s3)
+    lw t1, 0(s4)
+    mul t0, t0, t1
+    slli s5, t0, 2
+
+    # malloc for matrix
+    mv a0, s5
+    jal ra, malloc
+    mv s6, a0
+
+    # read matrix
+    mv a1, s0
+    mv a2, s6
+    mv a3, s5
+    jal ra, fread  
+
+    mv a0, s6
+    lw t0, 0(s3)
+    sw t0, 0(s1)
+    lw t0, 0(s4)
+    sw t0, 0(s2)
 
 
     # Epilogue
-
+    # pop ra, s0, s1, s2, s3, s4, s5, s6
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+    lw s5, 24(sp)
+    lw s6, 28(sp)
+    addi sp, sp, 32
 
     ret
