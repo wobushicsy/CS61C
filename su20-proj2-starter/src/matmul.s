8ce8c36d86b90809eea9aebe7d45abb7144f3f30
wobushicsy <csy657703828@163.com>
2023-04-16 14:51:03 +0800

    matmul fixed

--- su20-proj2-starter/src/matmul.s
+++ su20-proj2-starter/src/matmul.s
@@ -32,7 +32,16 @@ matmul:
     bne a2, a4, exit_4
 
     # Prologue
-    # push s0, s1, s2, s3, s4, s5, s6
+	# push ra, s0, s1, s2, s3, s4, s5, s6
+    addi sp, sp, -32
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+    sw s2, 12(sp)
+    sw s3, 16(sp)
+    sw s4, 20(sp)
+    sw s5, 24(sp)
+    sw s6, 28(sp)
 
     # s0, s1 represent outer, inner loop counter
     mv s0, x0
@@ -113,7 +122,16 @@ outer_loop_end:
 
 
     # Epilogue
-    # pop 
+    # pop ra, s0, s1, s2, s3, s4, s5, s6
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+    lw s5, 24(sp)
+    lw s6, 28(sp)
+    addi sp, sp, 32
 
     ret
 
